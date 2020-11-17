27e149718f41d864645f34773cdccb974a86bb34
강현혜 <74286526+HK59@users.noreply.github.com>
2020-11-17 11:42:22 +0900

    20201117 Break 예시

---
+++ src/com/javaex/ex04/Ex08.java
@@ -0,0 +1,23 @@
+package com.javaex.ex04;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		//*예제 1 	
+		int num = 1;
+		
+		while(true)	 {
+			 
+			if (num%6==0 && num%14==0) {
+			System.out.println(num);
+			break;
+		
+			}
+		num++;
+		}
+		
+	}
+
+}
---
+++ src/com/javaex/ex04/Ex09.java
@@ -0,0 +1,17 @@
+package com.javaex.ex04;
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int no=1;no<=100; no++) {
+			if (no%6==0 && no%14==0){
+				System.out.println(no);
+				break;
+			}
+		
+	}
+	}
+}
+
